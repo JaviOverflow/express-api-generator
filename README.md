a6aa6f0d9aa72ec234b4cc3323d6e9da7017fa9c
JaviOverflow <javioverflow@gmail.com>
2017-01-17 12:04:01 +0100

    Examples added and some refactoring

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # express-api-generator
+
+Generate '/projects' endpoint with all methods (POST, GET, UPDATE, DELETE) and its use cases
+```bash
+$ apigen --name projects --all
+or
+$ apigen --name projects -crud
+
+$ apigen -n projects -crud
+or
+$ apigen --name projects -crud
+```
+
+Generate '/logs' endpoint with the following methods (GET, POST) and its use cases
+```bash
+$ apigen --name logs -cr
+or
+$ apigen -n logs -cr
+```
--- index.js
+++ index.js
@@ -7,26 +7,20 @@ main(argv);
 function main({n, name, all, c, r, u, d} = null) {
 
     let entityName = (n !== undefined) ? n : name;
-    let includes = generateEndpointsIncludes(entityName, all, c, r, u, d);
-    let implementations = generateEndpointsImplementations(entityName, all, c, r, u, d);
-    let endpointsFile = generateEndpointsFile(entityName, includes, implementations);
 
-    fs.writeFile(`${entityName}.js`, endpointsFile, (err) => {});
+    generateEndpointsFile(entityName, all, c, r, u, d);
+    generateUseCasesFiles(entityName, all, c, r, u, d);
+}
+function generateEndpointsFile(entityName, all, c, r, u, d) {
+    let includes = fillEndpointsIncludes(entityName, all, c, r, u, d);
+    let implementations = fillEndpointsImplementations(entityName, all, c, r, u, d);
+    let endpointsFile = fillEndpointsTemplate(entityName, includes, implementations);
 
-    const useCases = [
-        { mustBeGenerated: true, filledTemplate: templates.FILE_USECASE_GET_ALL(entityName), filename: `${entityName}Get.js` },
-        { mustBeGenerated: r, filledTemplate: templates.FILE_USECASE_GET_SINGLE(entityName), filename: `${entityName}SingleGet.js` },
-        { mustBeGenerated: c, filledTemplate: templates.FILE_USECASE_POST_SINGLE(entityName), filename: `${entityName}SingleCreate.js` },
-        { mustBeGenerated: u, filledTemplate: templates.FILE_USECASE_UPDATE_SINGLE(entityName), filename: `${entityName}SinglePut.js` },
-        { mustBeGenerated: d, filledTemplate: templates.FILE_USECASE_DELETE_SINGLE(entityName), filename: `${entityName}SingleDelete.js` },
-    ];
-
-    useCases
-        .filter(useCase => useCase.mustBeGenerated)
-        .forEach(useCase => fs.writeFile(useCase.filename, useCase.filledTemplate, (err) => {}));
+    fs.writeFile(`${entityName}.js`, endpointsFile, (err) => {
+    });
 }
 
-function generateEndpointsIncludes(entityName, all, create, read, update, delete_) {
+function fillEndpointsIncludes(entityName, all, create, read, update, delete_) {
     let includes = [];
     includes.push(templates.INCLUDE_GET_ALL(entityName));
     if (all || create) includes.push(templates.INCLUDE_POST_SINGLE(entityName));
@@ -36,7 +30,7 @@ function generateEndpointsIncludes(entityName, all, create, read, update, delete
     return includes;
 }
 
-function generateEndpointsImplementations(entityName, all, create, read, update, delete_) {
+function fillEndpointsImplementations(entityName, all, create, read, update, delete_) {
     let implementations = [];
     implementations.push(templates.ENDPOINT_GET_ALL(entityName));
     if (all || create) implementations.push(templates.ENDPOINT_POST_SINGLE(entityName));
@@ -46,9 +40,49 @@ function generateEndpointsImplementations(entityName, all, create, read, update,
     return implementations;
 }
 
-function generateEndpointsFile(entityName, includes, implementations) {
+function fillEndpointsTemplate(entityName, includes, implementations) {
     return templates.FILE_ENDPOINT(
         entityName,
         includes.join('\n'),
         implementations.join('\n'));
+}
+
+function generateUseCasesFiles(entityName, all, c, r, u, d) {
+    const useCases = fillUseCasesTemplates(entityName, all, c, r, u, d);
+
+    useCases
+        .filter(useCase => useCase.mustBeGenerated)
+        .forEach(useCase => fs.writeFile(useCase.filename, useCase.filledTemplate, (err) => {
+        }));
+}
+
+function fillUseCasesTemplates(entityName, all, c, r, u, d) {
+    const useCases = [
+        {
+            mustBeGenerated: true,
+            filledTemplate: templates.FILE_USECASE_GET_ALL(entityName),
+            filename: `${entityName}Get.js`
+        },
+        {
+            mustBeGenerated: all || c,
+            filledTemplate: templates.FILE_USECASE_POST_SINGLE(entityName),
+            filename: `${entityName}SingleCreate.js`
+        },
+        {
+            mustBeGenerated: all || r,
+            filledTemplate: templates.FILE_USECASE_GET_SINGLE(entityName),
+            filename: `${entityName}SingleGet.js`
+        },
+        {
+            mustBeGenerated: all || u,
+            filledTemplate: templates.FILE_USECASE_UPDATE_SINGLE(entityName),
+            filename: `${entityName}SinglePut.js`
+        },
+        {
+            mustBeGenerated: all || d,
+            filledTemplate: templates.FILE_USECASE_DELETE_SINGLE(entityName),
+            filename: `${entityName}SingleDelete.js`
+        },
+    ];
+    return useCases;
 }
